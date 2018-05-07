e79d538cf3398d33de998fa5a09f0344f397e076
wangxiaoqing <xiaoqingwang@126.com>
2018-05-07 20:39:47 +0800

    excel

--- app/src/components/importexcel/exceltable.js
+++ app/src/components/importexcel/exceltable.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
+import EditableCell from './edittablecell';
 /*
   Simple HTML Table
   usage: <OutTable data={data} cols={cols} />
@@ -9,29 +11,45 @@ import React, { Component } from 'react';
 class OutTable extends React.Component {
 	constructor(props) {
 		super(props);
-	};
+		const {data} = this.props;
+		let dataSource = [];
+		if(data.length > 1){
+			for(let i = 1 ;i < data.length ; i ++){
+				let dataitem = {
+					key:`${i}`
+				};
+				for(let j = 0 ;j < data[0].length ;j ++){
+					dataitem[data[0][j]] = data[i][j];
+				}
+				dataSource.push(dataitem)
+			}
+		}
+		this.state = {
+			dataSource
+		}
+	}
+
 	render() {
-		const {cols,data} = this.props;
-		console.log(data);
-		console.log(cols);
+		const {data} = this.props;
+		const {dataSource} = this.state;
+
+		let columns = [];
+		if(data.length > 0){
+			for(let i = 0 ;i < data[0].length ;i ++){
+				columns.push({
+					 title: data[0][i],
+					 dataIndex: data[0][i],
+					 key: data[0][i],
+				})
+			}
+		}
+
+		console.log(columns);
+		console.log(dataSource);
+
     return (
         <div className="table-responsive">
-        	<table className="table table-striped">
-        		{/* <thead>
-        			<tr>{cols.map((c) => <th key={c.key}>{c.name}</th>)}</tr>
-        		</thead> */}
-        		<tbody>
-        			{
-								data.map((r,i) =>
-										<tr key={i}>
-		        				{
-											cols.map(c => <td key={c.key}>{ r[c.key] }</td>)
-										}
-		        			</tr>
-								)
-							}
-        		</tbody>
-        	</table>
+        	<Table dataSource={dataSource} columns={columns} />
         </div>
   	);
   };
